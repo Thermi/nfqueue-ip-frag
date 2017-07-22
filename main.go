e3adff6ed1d993525dc3636631fdcaa33fdd3c94
Noel Kuntze <noel@familie-kuntze.de>
2017-07-22 13:05:34 +0200

    Parse arguments

--- main.go
+++ main.go
@@ -15,5 +15,11 @@ type arguments struct {
 
 func main() {
 	// parse args
+	var args arguments
+	flag.Uint64Var(&args.concurrency, "concurrency", 1, "The number of concurrent go routines to work on fragmenting packets")
+	flag.Uint64Var(&args.queueNumber, "queueNumber", 1, "The nfqueue number that this application should use")
+	flag.BoolVar(&args.verbose, "verbose", false, "Enable or disable verbose mode")
+
+	flag.Parse()
 
 }
