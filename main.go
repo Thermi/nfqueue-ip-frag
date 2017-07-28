36bcf6baac6e44151388caea38ee0c8a7fd28bf6
Noel Kuntze <noel@familie-kuntze.de>
2017-07-28 02:40:06 +0200

    Implement packet fragmentation - implement socket struct for MT safe sending of packets - implement function to send packets MT safe - implement packet fragmentation in workers - implement sending of fragments - optimize worker loop a bit - reduce size of first packet to hit set MTU - open raw socket - set sockopt IP_MTU_DISCOVER to 0 (disable MTU discovery)

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"container/list"
 //	"errors"
 	"fmt"
+	"math"
 	"os"
 	"os/signal"
 	"syscall"
@@ -21,8 +23,14 @@ type arguments struct {
 	mtu uint64
 }
 
+type rawSocket struct {
+	fd 			int
+	lock 		*sync.Mutex
+
+}
 var wg sync.WaitGroup
 var args arguments
+var socket rawSocket
 
 // Set a static MTU for now. Later, this has to be dynamic and depending on the destination
 var defaultMtu uint64 = 1300
@@ -78,17 +86,20 @@ func receivePackets(payload *nfqueue.Payload) error {
 
 func processPackets() {
 	var packet *nfqueue.Payload
-	var newPacket []byte
+	var ipv4 layers.IPv4
+
+	listOfFragments := list.New()
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeIPv4, &ipv4)
+
 	for {
 		packet = <- packetChannel
 		if packet == nil {
 			return
 		}
-		var ipv4 layers.IPv4
+
 
 		// Decode the original packet and take its IP header
 
-		parser := gopacket.NewDecodingLayerParser(layers.LayerTypeIPv4, &ipv4)
 		decoded := []gopacket.LayerType{}
 		
 		err := parser.DecodeLayers(packet.Data, &decoded)
@@ -96,7 +107,7 @@ func processPackets() {
 			fmt.Println("Could not decode packet.")
 			continue
 		}
-		data := ipv4.Payload
+
 		// Set more fragments
 		// Check if don't fragment is set:
 		if ipv4.Flags & layers.IPv4DontFragment > 0 {
@@ -112,17 +123,19 @@ func processPackets() {
 		headerLength := len(ipv4.Contents)
 		
 		// This is the length of payload section of the new original IP packet
-		var newLength uint64 = args.mtu - uint64(headerLength)
+		var firstFragmentPayloadLength uint64 = args.mtu - uint64(headerLength)
 		// Split payload at the MTU
 		// set length of the IP packet
 
-		ipv4.Length = uint16(newLength)
+		ipv4.Length = uint16(firstFragmentPayloadLength)
 
 		layerBuffer := gopacket.NewSerializeBuffer()
 		// We need to write the payload first
-		bytes, _ := layerBuffer.PrependBytes(int(newLength))
-		copy(bytes, ipv4.Payload[:newLength-1])
-		_  = ipv4.SerializeTo(layerBuffer, gopacket.SerializeOptions{})
+		bytes, _ := layerBuffer.PrependBytes(int(firstFragmentPayloadLength))
+
+		// Let's hope the offset of one is correct
+		copy(bytes, ipv4.Payload[:firstFragmentPayloadLength-1])
+		_  = ipv4.SerializeTo(layerBuffer, gopacket.SerializeOptions{ComputeChecksums : true})
 		
 
 		// Set verdict on the original packet, but with the new data.
@@ -133,32 +146,77 @@ func processPackets() {
 			fmt.Println("An error occured setting the verdict for packet ", ipv4.Id, " - ", err)
 		}
 
+		// The rest length of the IP packet we need to put into fragments
+		var alreadySentBytes uint64 = firstFragmentPayloadLength
 
-		// The rest length of the IP packet we need to 
-		var restLength uint64 = uint64(len(data)) - newLength
+		numberOfFragments := int(math.Ceil(float64(len(ipv4.Payload)) / float64(args.mtu)))
 
-
+		var newPayloadLength uint64
 		// calculate the number of other fragments we need to send
+		for i := 0; i< numberOfFragments; i++ {
+			// clear the layerBuffer
+			layerBuffer.Clear()
+			// clear the slice
+			bytes = nil
+			
+			ipv4.FragOffset = uint16(alreadySentBytes)
+			// If this is the last fragment, we need to set some special bits
+			if (i + 1 == numberOfFragments) {
+				newPayloadLength = uint64(len(ipv4.Payload)) - alreadySentBytes
+				// set no more fragments
+				ipv4.Flags ^= layers.IPv4MoreFragments
+			} else {
+				// This is an intermediate fragment, in which we can put the maximum amount of bytes
+				// into the packet up until the mtu is reached.
+				newPayloadLength = firstFragmentPayloadLength
 
+			}
+			bytes = make([]byte, newPayloadLength)
+			
+			copy(bytes, ipv4.Payload[alreadySentBytes:newPayloadLength])
 
-		// Set verdict on the original packet, but with the new data.
-		// Pass pointer to new buffer here
-		//nfqueue.SetVerdictModified(nfqueue.NF_ACCEPT, newPacket)
+			alreadySentBytes += firstFragmentPayloadLength
 
-		// Calculate how many fragments we actually have to make
-		// Create the corpus for a new IP packet
-		// Set fragment offset and length
-		// gopacket.NewPacket(...)
-		//
-		// gopacket.SerializePacket(...)
-		
-		//
-		// func (p *Payload) SetVerdictModified(verdict int, data []byte) error {
+			_  = ipv4.SerializeTo(layerBuffer, gopacket.SerializeOptions{ComputeChecksums : true})
+			
+			listOfFragments.PushBack(layerBuffer.Bytes())
+		}
+
 		// Write fragments to raw socket
+		// https://www.darkcoding.net/software/raw-sockets-in-go-link-layer/
+		// man 7 raw
+		
+		err = sendPackets(listOfFragments)
+		// Clear the list
+		listOfFragments.Init()
 	}
 
 }
 
+func sendPackets(packetList *list.List) error {
+	var packet		[]byte
+	var err 		error
+	addr := syscall.SockaddrInet4{
+		Port: 0,
+		Addr: [4]byte{127, 0, 0, 1},
+	}
+
+	socket.lock.Lock()
+	defer socket.lock.Unlock()
+
+	for e := packetList.Front(); e != nil; e = e.Next() {
+		// destination is irrelevant here, because the socket takes over
+		// the whole IP header of our packet in the byte array
+		packet = e.Value.([]byte)
+		err = syscall.Sendto(socket.fd, packet, 0, &addr)
+		if err != nil {
+			fmt.Println("An error occured when sending a packet: ", err)
+			return err
+		}
+	}
+
+	return nil
+}
 func main() {
 	// parse args
 	var err error
@@ -169,6 +227,24 @@ func main() {
 	flag.BoolVar(&args.verbose, "verbose", false, "Enable or disable verbose mode")
 
 	flag.Parse()
+
+	// open the raw socket
+	// We only want to send!
+	socket.fd, err = syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+
+	if err != nil {
+		fmt.Println("An error occurred when the socket was opened: ", err)
+		os.Exit(1)
+	}
+
+	// We know better. :)
+	err = syscall.SetsockoptInt(socket.fd, syscall.SOL_SOCKET, syscall.IP_MTU_DISCOVER, 0)
+	if err != nil {
+		fmt.Println("An error occurred when the socket option IP_MTU_DISCOVER was set: ", err)
+		os.Exit(1)
+	}
+
+	socket.lock = new(sync.Mutex)
 
 	var q = new(nfqueue.Queue)
 	// Implicitely starts one function, account for when starting q.Loop()
